22e63ffe83cb3aa031f161e4dcbaba0a3fea52d7
Mario Juguilon <mariojjuguilon@gmail.com>
2021-11-02 19:07:34 -0500

    Add templates

---
+++ Diplomacy.py
@@ -0,0 +1,36 @@
+army_info = []
+
+# Army class, keeps track of army name, let location and action
+# be an attribute of Army. Maybe use string function to display army
+# and location
+
+def diplomacy_read(s):
+    '''
+    read list of strings
+    s a list of strings
+    '''
+    data = [str(i) for i in s.split()]
+
+    # insert code to put data in an Army class object
+    # put Army object into armyInfo
+
+def diplomacy_print(w):
+    '''
+    w a writer
+    takes data from army_info and army name and their locations
+    '''
+    pass
+
+def diplomacy_eval():
+    '''
+    evaluates all data from army_info and updates location attributes of the army objects
+    '''
+    pass
+
+def diplomacy_solve(r, w):
+    '''
+    r a reader
+    w a writer
+    reads all input from the file and executes Diplomacy
+    '''
+    pass
---
+++ TestDiplomacy.py
@@ -0,0 +1,19 @@
+from io import StringIO
+from unittest import main, TestCase
+
+from Diplomacy import diplomacy_read, diplomacy_eval, diplomacy_print, diplomacy_solve
+
+class TestDiplomacy (TestCase):
+    # ----
+    # read
+    # ----
+    pass
+
+
+# ----
+# main
+# ----
+
+
+if __name__ == "__main__":
+    main()
